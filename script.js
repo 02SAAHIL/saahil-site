37b392fe3f25c40bd041f52f5570e141244ee6b3
Saahil verma <99863074+02SAAHIL@users.noreply.github.com>
2026-04-08 00:49:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// Typing effect
+const text = ["Data Analyst", "Python Developer", "AI Enthusiast"];
+let i = 0;
+let j = 0;
+let currentText = "";
+let isDeleting = false;
+
+function type() {
+  if (i < text.length) {
+    if (!isDeleting && j <= text[i].length) {
+      currentText = text[i].substring(0, j++);
+    } else if (isDeleting && j >= 0) {
+      currentText = text[i].substring(0, j--);
+    }
+
+    document.querySelector(".typing").textContent = currentText;
+
+    if (j === text[i].length) isDeleting = true;
+    if (j === 0) {
+      isDeleting = false;
+      i = (i + 1) % text.length;
+    }
+  }
+  setTimeout(type, 100);
+}
+
+type();
